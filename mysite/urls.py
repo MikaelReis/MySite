3c659f8592b1220063cfae5f369021193c979884
MikaelReis <mikaelreis08@gmail.com>
2025-04-25 12:17:16 -0300

    realizando ajustes

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,12 +1,9 @@
 """
 URL configuration for mysite project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-<<<<<<< HEAD
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-=======
+The `urlpatterns` list routes URLs to views. 
+For more information, please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
->>>>>>> a993f214c704b874056ff7b22412a018e1702d17
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,20 +13,13 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-<<<<<<< HEAD
-    2. Add a URL to urlpatterns:  path('blog_old/', include('blog_old.urls'))
-=======
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
->>>>>>> a993f214c704b874056ff7b22412a018e1702d17
+    2. Add a URL to urlpatterns:  path('home/', include('blog_app.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-<<<<<<< HEAD
-    path('home/', include('blog_app.urls')),  # aponta pro app novo
-=======
-    path('home', include('blog.urls')),
->>>>>>> a993f214c704b874056ff7b22412a018e1702d17
+    path('home/', include('blog_app.urls')),  # apontando para o novo app
 ]
